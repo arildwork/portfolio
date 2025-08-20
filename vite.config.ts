b7ffb20717fb9b5ad81220c26242b0e84701d3af
Arild Sela <arild.sela@sequello.com>
2025-08-20 15:48:34 +0200

    Added vite config for baseUrl

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,41 @@
-import { defineConfig } from "vite";
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/portfolio/",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const baseURL = env.VITE_BASE_URL;
+
+  return {
+    root: __dirname,
+    plugins: [react()],
+    base: baseURL,
+    build: {
+      outDir: "dist",
+      emptyOutDir: true,
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+    server: {
+      port: 5050,
+      strictPort: true,
+      cors: true,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    },
+    preview: {
+      port: 5050,
+      strictPort: true,
+    },
+    resolve: {
+      alias: {
+        "@components": path.resolve(__dirname, "./src/components"),
+        "@assets": path.resolve(__dirname, "./src/assets"),
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
 });
